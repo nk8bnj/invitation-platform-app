d0c26b8cd035e573b0626530b36a2b580aba825e
Nikita Ganzha <nk8bnj@gmail.com>
2024-06-21 09:09:54 +0300

    Updates

--- server/server-reference-manifest.js
+++ server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"ICZtosu2FHgozuGbH29L9WRhmbaFbEfXd3pXvc8LjAk=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"NRBEfKDbZmdTTbVtcUYS+uwB+e/pD+hCZt/NDsRsERc=\"}"
